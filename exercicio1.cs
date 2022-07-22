33f89380485e09838f53da248d7d546821c73807
danolibel <109608641+danolibel@users.noreply.github.com>
2022-07-22 16:46:17 -0300

    Update exercicio1.cs

--- exercicio1.cs
+++ exercicio1.cs
@@ -1,18 +1,18 @@
 using System;
-
+// exercicio dominó
 public  class Program{
         public static void Main(string[] args)
 {
     int N,S;
     Console.WriteLine("Digite o valor N de circulos:");
 			N=int.Parse(Console.ReadLine());
-			if (N>=0){
-				if(N<=10000){
+			if (N>=0 && N<=10000){
+				
 			S=((N+1)*(N+2))/2;
 				Console.WriteLine(S);
-							}
+							
 					}else{
-			Console.WriteLine("Digite um numero entre o e 10.000");
+			Console.WriteLine("Digite um numero entre 0 e 10.000");
 						}
 				
 		}
